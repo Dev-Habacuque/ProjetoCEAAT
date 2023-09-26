e71457bf6b15244c0d8b2ae3f733a6e8cb47419e
Jeremias Habacuque <jeremiashabacuque321@gmail.com>
2023-09-26 11:47:03 -0300

    alpha1.3
    Implementar opções

--- main.js
+++ main.js
@@ -3,5 +3,9 @@ function inPrSof(){
 }
 
 function reDeFis(){
-    document.getElementById("activities").innerHTML = '<h5>Faça um breve resumo do problema apresentado</h5> <br> <form><input></input><input type="submit"</form>';
+    document.getElementById("activities").innerHTML = '<h3>Faça um breve resumo do problema apresentado</h3> <br> <form><textarea></textarea><br><input type="submit"></form>';
+}
+function inNoAp(){
+    document.getElementById("activities").innerHTML = '<h3>Selecione qual dispositivo será instalado:</h3> <br> <form><br><input type="submit"></form>';// Adicionar opções
+
 }
