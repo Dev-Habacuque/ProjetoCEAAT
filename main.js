0ed453494a9eefe5be5b2236f327133366816556
Jeremias Habacuque <112903643+Dev-Habacuque@users.noreply.github.com>
2023-09-26 09:17:10 -0300

    alpha 1.2

---
+++ main.js
@@ -0,0 +1,7 @@
+function inPrSof(){
+    document.getElementById("activities").innerHTML = '<h3>Qual o nome do programa?</h3> <br> <form><input></input><input type="submit"</form>';
+}
+
+function reDeFis(){
+    document.getElementById("activities").innerHTML = '<h5>Faça um breve resumo do problema apresentado</h5> <br> <form><input></input><input type="submit"</form>';
+}
