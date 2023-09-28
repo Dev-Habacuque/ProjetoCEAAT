44a0e11146c467aa89551b5ffad4ad1891615fea
Jeremias Habacuque <jeremiashabacuque321@gmail.com>
2023-09-28 11:27:18 -0300

    alpha 1.5

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const mainText = '<h2>Solicitação:</h2><ol><li onclick="inPrSof()">Instalação de Programa/Software.</li><li onclick="reDeFis()">Reparação de defeito físico.</li><li onclick="inNoAp()">Instalação de novo aparelho.</li><li onclick="coImSc()">Configuração de Impressora/Scan</li></ol>'
 
+
 function inPrSof(){
     document.getElementById("activities").innerHTML = '<h3>Qual o nome do programa?</h3> <br> <form><input type="text"><br><input type="submit" onclick="goBack()" value="Voltar"><input type="submit"></form>';
 }
@@ -8,7 +9,7 @@ function reDeFis(){
     document.getElementById("activities").innerHTML = '<h3>Faça um breve resumo do problema apresentado</h3> <br> <form><textarea></textarea><br><input type="submit" onclick="goBack()" value="Voltar"><input type="submit"></form>';
 }
 function inNoAp(){
-    document.getElementById("activities").innerHTML = '<h3>Selecione qual dispositivo será instalado:</h3> <br> <form><br> <input type="text"><br><input type="submit"><input type="submit" onclick="goBack()" value="Voltar"></form>';// Adicionar opções
+    document.getElementById("activities").innerHTML = '<h3>Selecione qual dispositivo será instalado:</h3> <br> <form><br> <input type="text"><br><input type="submit" onclick="goBack()" value="Voltar"><input type="submit"></form>';// Adicionar opções
 
 }
 function coImSc(){
