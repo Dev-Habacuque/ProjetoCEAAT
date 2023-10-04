610b33c7108ab867ce9381ee3aa29f4535e6e33c
Jeremias Habacuque <jeremiashabacuque321@gmail.com>
2023-10-04 11:44:47 -0300

    Alpha 2.1

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const mainText = '<p>Solicitação:</p><ol><li onclick="inPrSof()">Instalação de Programa/Software.</li><li onclick="reDeFis()">Reparação de defeito físico.</li><li onclick="inNoAp()">Instalação de novo aparelho.</li><li onclick="coImSc()">Configuração de Impressora/Scan</li></ol>'
 
 var menuButton = document.getElementById("menu-button");
 var menu = document.getElementById("menu");
@@ -26,10 +25,10 @@ function coImSc() {
   document.getElementById("activities").innerHTML = '<p>Faça um breve resumo do problema apresentado</p> <form id="default"> <input type="text"> <br><input type="submit" onclick="goBack()" value="Voltar"><input type="submit"></form>';
 }
 function goBack() {
-  document.getElementById("activities").innerHTML = mainText
+  document.getElementById("activities").reload()
 
 }
 function aboutCreators() {
-  document.getElementById("activities").innerHTML = '<h3>Sobre o Desenvolvimento</h3><p>Site desenvolvido integralmente surporte técnico para colaboradores da escola e fins institucionais  </p><h3>Lista de Developers:</h3><p><ul><li>Victor Trindade Cerqueira</li><li>Jeremias Habacuque</li></ul></p><h3>Integrantes do projeto</h3><p><ul><li>Antonio Carlos</li><li>Gabriel Costa</li><li>Geisiane Dias</li><li>Felipe Souza</li></ul</p>'
+  document.getElementById("activities").innerHTML = '<div class="acontainer"><h3>Sobre o Desenvolvimento</h3><h4 id="aboutText">Site desenvolvido integralmente surporte técnico para colaboradores da escola e fins institucionais  </h4><h3>Lista de Developers:</h3><p><ul><li>Victor Trindade Cerqueira</li><li>Jeremias Habacuque</li></ul></p><h3>Integrantes do projeto</h3><p><ul><li>Antonio Carlos</li><li>Gabriel Costa</li><li>Geisiane Dias</li><li>Felipe Souza</li></ul</p></div>'
 }
 
