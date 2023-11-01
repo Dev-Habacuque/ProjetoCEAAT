5c920ebb8ab66a38801a595278d7701e9e189215
Jeremias Habacuque <jeremiashabacuque321@gmail.com>
2023-11-01 11:22:57 -0300

    Alpha 3.0

--- main.js
+++ main.js
@@ -24,11 +24,12 @@ function inNoAp() {
 function coImSc() {
   document.getElementById("activities").innerHTML = '<br><div class="mcontainer"><p>Faça um breve resumo do problema apresentado</p> <form id="default"> <input type="text"><br><button type="submit" onclick="goBack()">Voltar</button><button type="submit">Enviar</button></form></div>';
 }
-function goBack() {
-  document.getElementById("activities").reload()
-
-}
 function aboutCreators() {
   document.getElementById("activities").innerHTML = '<div class="acontainer"><h3>Sobre o Desenvolvimento</h3><h4 id="aboutText">Site desenvolvido integralmente surporte técnico para colaboradores da escola e fins institucionais  </h4><h3>Lista de Developers:</h3><p><ul><li>Victor Trindade Cerqueira</li><li>Jeremias Habacuque</li></ul></p><h3>Integrantes do projeto</h3><p><ul><li>Antonio Carlos</li><li>Gabriel Costa</li><li>Geisiane Dias</li><li>Felipe Souza</li></ul</p></div>'
 }
+var btn = document.querySelector("#refresh");
+btn.addEventListener("click", function() {
+    
+    location.reload();
+});
 
