82098932aeb69344060ede7ff6fdd6af8e515783
Bigcupcake364 <victortrindade364@gmail.com>
2023-11-02 16:23:49 -0300

    Alpha final

--- main.js
+++ main.js
@@ -9,27 +9,3 @@ menuButton.addEventListener("click", function () {
     menu.style.display = "block";
   }
 });
-
-function inPrSof() {
-  document.getElementById("activities").innerHTML = '<br><div class="mcontainer"><p>Qual o nome do programa?</p> <form id="default"> <input type="text"><br> <button type="submit" onclick="goBack()">Voltar</button><button type="submit">Enviar</button></form></div>';
-}
-
-function reDeFis() {
-  document.getElementById("activities").innerHTML = '<br><div class="mcontainer"><p>Faça um breve resumo do problema apresentado</p> <form id="default"><textarea rows="10" cols="50"></textarea><br><button type="submit" onclick="goBack()">Voltar</button><button type="submit">Enviar</button></form></div>';
-}
-function inNoAp() {
-  document.getElementById("activities").innerHTML = '<br><div class="mcontainer"><p>Selecione qual dispositivo será instalado:</p> <form id="default"> <input type="text"><br><button type="submit" onclick="goBack()">Voltar</button><button type="submit">Enviar</button></form></div>';// Adicionar opções
-
-}
-function coImSc() {
-  document.getElementById("activities").innerHTML = '<br><div class="mcontainer"><p>Faça um breve resumo do problema apresentado</p> <form id="default"> <input type="text"><br><button type="submit" onclick="goBack()">Voltar</button><button type="submit">Enviar</button></form></div>';
-}
-function aboutCreators() {
-  document.getElementById("activities").innerHTML = '<div class="acontainer"><h3>Sobre o Desenvolvimento</h3><h4 id="aboutText">Site desenvolvido integralmente surporte técnico para colaboradores da escola e fins institucionais  </h4><h3>Lista de Developers:</h3><p><ul><li>Victor Trindade Cerqueira</li><li>Jeremias Habacuque</li></ul></p><h3>Integrantes do projeto</h3><p><ul><li>Antonio Carlos</li><li>Gabriel Costa</li><li>Geisiane Dias</li><li>Felipe Souza</li></ul</p></div>'
-}
-var btn = document.querySelector("#refresh");
-btn.addEventListener("click", function() {
-    
-    location.reload();
-});
-
